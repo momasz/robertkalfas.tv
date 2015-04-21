24e9912bf5f5ad6065ec02db27e16760306f2629
tomasz_kalfas <tomasz.kalfas@schibsted.pl>
2015-04-21 09:14:57 +0200

    google analytics implemented, new video fetcher moved to backend app

--- src/app/config.js
+++ src/app/config.js
@@ -7,7 +7,7 @@ angular.module('robertkalfas')
       apiSimpleUrl: 'https://vimeo.com/api/v2/',
       apiToken: 'ad142b363bbe2b9e51b478f7fa908d6a',
       playerUrl: 'https://player.vimeo.com/video/',
-      sourceUrl: 'http://portfolio-cms.herokuapp.com/config',
+      sourceUrl: 'http://portfolio-cms.herokuapp.com/videos',
       perPage: 10
     };
   });
